7acbd4a1a84e4206dc4d530c5dcf7726bcc9ac63
ioedumac <eacevedof@yahoo.es>
2020-03-17 15:35:13 +0100

    ya encuentra el num de caminos

--- udemy_cssgrid_flexbox/bfs.js
+++ udemy_cssgrid_flexbox/bfs.js
@@ -1,6 +1,44 @@
 //bfs (Bread First Search) o busqueda en anchura
 //https://www.youtube.com/watch?v=-qX4lodQ920 Matriz de adyacenci
 // Total de caminos diferentes entre G https://youtu.be/-qX4lodQ920?t=243
+/*
+	¡El GPS se ha roto y tenemos que llegar a un sitio!
+
+	Por suerte, tienes un mapa y tu algoritmo sigue intacto. Eso sí, tu mapa es
+	un poco	extraño: todas las intersecciones están etiquetadas con números enteros
+	diferentes y las  carreteras que las conectan están etiquetadas con el tiempo
+	que se tarda en recorrerlas expresado en minutos.
+
+	Te encuentras en la intersección etiquetada como "start" y tu destino es la
+	interescción etiquetada como "finish".
+
+	Dispondrás del número total de intersecciones y un array de carreteras, cada
+	una de ellas con las propiedades: "from", "to" (las interesecciones están
+	etiquetadas con números enteros menores que el número de intersecciones) y
+	"drivingTime". Las carreteras sólo pueden ser usadas para ir desde "from" a
+	"to". No hay carreteras de doble sentido.
+
+	Completa la función para que devuelva un array de intersecciones de la ruta
+	más rápida desde "start" hasta "finish" (ambas incluidas).
+
+	Si hay vairas rutas iguales, coge cualquiera. Si no hay rutas, devuelve null.
+
+	Ejemplo:
+
+	var roads = [
+	    {from: 0, to: 1, drivingTime: 5},
+	    {from: 0, to: 2, drivingTime: 10},
+	    {from: 1, to: 2, drivingTime: 10},
+	    {from: 1, to: 3, drivingTime: 2},
+	    {from: 2, to: 3, drivingTime: 2},
+	    {from: 2, to: 4, drivingTime: 5},
+	    {from: 3, to: 2, drivingTime: 2},
+	    {from: 3, to: 4, drivingTime: 10}
+	];
+	navigate(5, roads, 0, 4);
+	// devolvería [0, 1, 3, 2, 4]. Tiempo más rápido is 5 + 2 + 2 + 5 = 14 minutes
+
+*/
 const roads = [
 	{from: 0, to: 1, drivingTime: 5},
 	{from: 0, to: 2, drivingTime: 10},
@@ -86,7 +124,7 @@ const _get_path = (istart)=>{
 }
 //hay arista
 const _is_edge = (istart,iend,roads)=>{
-  const arres = roads.filter(obj => obj.from==istart && obj.from==iend)
+  const arres = roads.filter(obj => obj.from==istart && obj.to==iend)
   return arres.length>0
 }
 
@@ -106,8 +144,8 @@ const _get_adyacency = (roads)=>{
 
 
 const _get_product = (m1,m2)=>{
-  console.table(m1)
-  console.table(m2)
+  //console.table(m1)
+  //console.table(m2)
   let newmat = []
   const ilenm1 = m1.length
   const ilenm2 = m2.length
@@ -134,10 +172,30 @@ const _get_product = (m1,m2)=>{
       newmat.push(resrow)
     }
   }
-  console.table(newmat)
+  //console.log("_get_product")
+  //console.table(newmat)
   return newmat
 }
 
+const _get_mat_potency = (m1,iexp)=>{
+  if(iexp==0) return 1
+  if(iexp==1) return m1
+  let result = m1
+  for(let i=0; i<(iexp-1); i++){
+    result = _get_product(result,m1)
+  }
+  console.log("_get_mat_potency",iexp)
+  console.table(result)
+  return result
+}
+
+const _get_num_roads = (inodes,roads,istart,ifinish)=>{
+  const adjmatrix = _get_adyacency(roads)
+  const resmat = _get_mat_potency(adjmatrix,inodes)
+  const total = resmat[istart][ifinish]
+  console.log("total roads",total)
+  return total
+}
 
 const istart = 0
 //_get_path(istart)
@@ -145,6 +203,10 @@ const istart = 0
 //console.log("archilds",archilds)
 
 //_get_product([[3,2],[1,4]],[[7,0],[5,6]])
-const matrix = _get_adyacency(roads)
-console.table(matrix)
+//const matrix = _get_adyacency(roads)
+//console.log("matix adjacency")
+//console.table(matrix)
+//_get_mat_potency(matrix,1)
+
 //_get_product([[3,2],[1,4]],[[7,0],[5,6]])
+_get_num_roads(5,roads,0,4)
